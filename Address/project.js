ba9ea03c9d8cf750c21134431d5d8afd9240006a
haraphat01 <haraphat@gmail.com>
2021-10-18 14:36:21 +0100

    added filter to the table

--- Address/project.js
+++ Address/project.js
@@ -1,13 +1,31 @@
 $("#submit").click(function(e){
     e.preventDefault();
+
+    function count(){
+        let c = 0;
+        let a = $("#table tr")
+        for(let i =0; a.length; i++){
+            c += 1
+        }
+
+        return c
+    }
     
     let firstName = $("#firstName").val();
     let lastName = $("#lastName").val();
     let phone1 = $("#phone").val();
     let address = $("#address").val();
-    let counter = 
-    $("#tbody").append(`<td>${firstName}</td><td>${lastName}</td><td>${phone1}</td><td>${address}</td>`);
+    let counter = count();
+    let destroy = 1
+    $("#tbody").append(`<tr><td>${counter}</td><td>${firstName}</td><td>${lastName}</td><td>${phone1}</td><td>${address}</td><td>${destroy}</td></tr>`);
     
    
 
+  });
+
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#tbody").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
   });
