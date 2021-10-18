752a6233b7d2eec040c6f5cfe329056866aa3372
haraphat01 <haraphat@gmail.com>
2021-10-18 12:40:54 +0100

    working on the form loop

--- Address/project.js
+++ Address/project.js
@@ -1,8 +1,12 @@
-$("#submit").click(function(){
+$("#submit").click(function(e){
+    e.preventDefault();
+    
     let firstName = $("#firstName").val();
     let lastName = $("#lastName").val();
-    let phone = $("#phone").val();
+    let phone1 = $("#phone").val();
     let address = $("#address").val();
+    let counter = 
+    $("#tbody").append(`<td>${firstName}</td><td>${lastName}</td><td>${phone1}</td><td>${address}</td>`);
     
    
 
